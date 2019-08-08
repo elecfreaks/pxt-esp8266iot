eb56ae99c65a0fc9510bafebb532161dc6fa4c88
LION-YAN\Lion <Lion_yan@outlook.com>
2019-08-08 18:10:37 +0800

    no message

--- ESP8266.ts
+++ ESP8266.ts
@@ -1,135 +1,116 @@
-
-/**
- * Use this file to define custom functions and blocks.
- * Read more at https://makecode.microbit.org
- */
-
-
-
 /**
  * Custom blocks
  */
 //% color=#0fbc11 icon="\uf1eb" weight=90
-
 namespace ESP8266_IoT {
-    let tobesendstring = ""
+
+   let wifi_connected: boolean = false
+    let thingspeak_connected: boolean = false
+    let last_upload_successful: boolean = false
+
+    // write AT command with CR+LF ending
+    function sendAT(command: string, wait: number = 100) {
+        serial.writeString(command + "\u000D\u000A")
+        basic.pause(wait)
+    }
+
+    // wait for certain response from ESP8266
+    function waitResponse(): boolean {
+        let serial_str: string = ""
+        let result: boolean = false
+        let time: number = input.runningTime()
+        while (true) {
+            serial_str += serial.readString()
+            if (serial_str.length > 200) serial_str = serial_str.substr(serial_str.length - 200)
+            if (serial_str.includes("OK") || serial_str.includes("ALREADY CONNECTED")) {
+                result = true
+                break
+            } else if (serial_str.includes("ERROR") || serial_str.includes("SEND FAIL")) {
+                break
+            }
+            if (input.runningTime() - time > 30000) break
+        }
+        return result
+    }
 
     /**
-     * TODO: Set pin RX and TX for ESP8266 Serial Wifi Module，Baud rate: 115200.
-     * @param wifiRX describe parameter here, eg: SerialPin.P8
-     * @param wifiTX describe parameter here, eg: SerialPin.P12
-     */
-    //% weight=100
-    //% blockId="wifi_init" block="set ESP8266 RX %wifiRX| TX %wifiTX|at baud rate %baudrate"
-    export function initwifi(wifiRX: SerialPin, wifiTX: SerialPin, baudrate: BaudRate): void {
+    * Initialize ESP8266 module and connect it to Wifi router
+    */
+    //% block="Initialize ESP8266|RX (Tx of micro:bit) %tx|TX (Rx of micro:bit) %rx|Baud rate %baudrate|Wifi SSID = %ssid|Wifi PW = %pw"
+    //% tx.defl=SerialPin.P8
+    //% rx.defl=SerialPin.P12
+    //% ssid.defl=your_ssid
+    //% pw.defl=your_pw
+    export function connectWifi(tx: SerialPin, rx: SerialPin, baudrate: BaudRate, ssid: string, pw: string) {
+        wifi_connected = false
+        thingspeak_connected = false
         serial.redirect(
-            wifiRX,
-            wifiTX,
-            //BaudRate.BaudRate115200
+            tx,
+            rx,
             baudrate
         )
-        basic.pause(10)
-        serial.writeString("AT+RST" + "\u000D" + "\u000A")
-        basic.pause(2000)
-        serial.writeString("AT+CWMODE=1" + "\u000D" + "\u000A")
-        basic.pause(5000)
-
-        // Add code here
+        sendAT("AT+RESTORE", 1000) // restore to factory settings
+        sendAT("AT+CWMODE=1") // set to STA mode
+        sendAT("AT+RST", 1000) // reset
+        sendAT("AT+CWJAP=\"" + ssid + "\",\"" + pw + "\"", 0) // connect to Wifi router
+        wifi_connected = waitResponse()
+        basic.pause(100)
     }
 
     /**
-     * TODO: connectwifi，Fill in your ssid and your key.
-     * @param ssid describe parameter here, eg: "your ssid"
-     * @param key describe parameter here, eg: "your key"
-     */
-    //% weight=99
-    //% blockId="wifi_connect" block="connect wifi SSID: %ssid| KEY: %key"
-    export function connectwifi(ssid: string, key: string): void {
-        // Add code here
-        let text = "AT+CWJAP=\""
-                 + ssid
-                 + "\",\""
-                 + key
-                 + "\""
-        
-        serial.writeString(text + "\u000D" + "\u000A")
-        basic.pause(6000)
-    }
-
-    /**
-     * TODO: connect thingspeak IoT TCP server 
+    * Connect to ThingSpeak and upload data. It would not upload anything if it failed to connect to Wifi or ThingSpeak.
     */
-    //% weight=98
-    //% blockId="TCP_connect" block="connect thingspeak"
-    export function connectthingspeak(): void {
-        // Add code here
-        let text = "AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80"
-        serial.writeString(text + "\u000D" + "\u000A")
-        basic.pause(6000)
+    //% block="Upload data to ThingSpeak|URL/IP = %ip|Write API key = %write_api_key|Field 1 = %n1|Field 2 = %n2|Field 3 = %n3|Field 4 = %n4|Field 5 = %n5|Field 6 = %n6|Field 7 = %n7|Field 8 = %n8"
+    //% ip.defl=api.thingspeak.com
+    //% write_api_key.defl=your_write_api_key
+    export function connectThingSpeak(ip: string, write_api_key: string, n1: number, n2: number, n3: number, n4: number, n5: number, n6: number, n7: number, n8: number) {
+        if (wifi_connected && write_api_key != "") {
+            thingspeak_connected = false
+            sendAT("AT+CIPSTART=\"TCP\",\"" + ip + "\",80", 0) // connect to website server
+            thingspeak_connected = waitResponse()
+            basic.pause(100)
+            if (thingspeak_connected) {
+                last_upload_successful = false
+                let str: string = "GET /update?api_key=" + write_api_key + "&field1=" + n1 + "&field2=" + n2 + "&field3=" + n3 + "&field4=" + n4 + "&field5=" + n5 + "&field6=" + n6 + "&field7=" + n7 + "&field8=" + n8
+                sendAT("AT+CIPSEND=" + (str.length + 2))
+                sendAT(str, 0) // upload data
+                last_upload_successful = waitResponse()
+                basic.pause(100)
+            }
+        }
     }
 
     /**
-     * TODO: Set data to be sent.
-     * @param write_api_key describe parameter here, eg: "your write api key"
-     * @param n1 describe parameter here, eg: 0
-     * @param n2 describe parameter here, eg: 0
-     * @param n3 describe parameter here, eg: 0
-     * @param n4 describe parameter here, eg: 0
-     * @param n5 describe parameter here, eg: 0
-     * @param n6 describe parameter here, eg: 0
-     * @param n7 describe parameter here, eg: 0
-     * @param n8 describe parameter here, eg: 0
-     */
-    //% weight=97
-    //% blockId="send_text" block="set data to be send : Write API Key= %write_api_key|field1 = %n1|field2 = %n2|field3 = %n3|field4 = %n4|field5 = %n5|field6 = %n6|field7 = %n7|field8 = %n8"
-    export function tosendtext(write_api_key: string,
-                                n1: number, 
-                                n2: number, 
-                                n3: number, 
-                                n4: number, 
-                                n5: number, 
-                                n6: number, 
-                                n7: number, 
-                                n8: number ): void {
-        let text=""   
-        text = "GET /update?key="
-            + write_api_key
-            + "&field1="
-            + n1
-            + "&field2="
-            + n2
-            + "&field3="
-            + n3
-            + "&field4="
-            + n4  
-            + "&field5="
-            + n5
-            + "&field6="
-            + n6
-            + "&field7="
-            + n7
-            + "&field8="
-            + n8  
-        tobesendstring = text              
-        // Add code here
+    * Wait between uploads
+    */
+    //% block="Wait %delay ms"
+    //% delay.min=0 delay.defl=5000
+    export function wait(delay: number) {
+        if (delay > 0) basic.pause(delay)
     }
 
     /**
-     * TODO: send data
-     */
-    //% weight=96
-    //% blockId=senddata block="send data to thingspeak"
-    export function senddata(): void {
-        let text = ""
-        text = "AT+CIPSEND=" 
-            + (tobesendstring.length + 2)
-        serial.writeString(text + "\u000D" + "\u000A")
-        basic.pause(3000)
-        serial.writeString(tobesendstring + "\u000D" + "\u000A")
-        basic.pause(6000)
-        // Add code here
-
+    * Check if ESP8266 successfully connected to Wifi
+    */
+    //% block="Wifi connected ?"
+    export function isWifiConnected() {
+        return wifi_connected
     }
 
+    /**
+    * Check if ESP8266 successfully connected to ThingSpeak
+    */
+    //% block="ThingSpeak connected ?"
+    export function isThingSpeakConnected() {
+        return thingspeak_connected
+    }
+
+    /**
+    * Check if ESP8266 successfully uploaded data to ThingSpeak
+    */
+    //% block="Last data upload successful ?"
+    export function isLastUploadSuccessful() {
+        return last_upload_successful
+    }
 
 }
