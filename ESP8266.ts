52377477101b347c5abd8cd1fe2223dd0d7e841e
Lionkk <Lion_yan@outlook.com>
2019-08-12 13:00:13 +0800

    Add files via upload

--- ESP8266.ts
+++ ESP8266.ts
@@ -7,6 +7,7 @@ namespace ESP8266_IoT {
     let wifi_connected: boolean = false
     let thingspeak_connected: boolean = false
     let last_upload_successful: boolean = false
+    let toSendStr = ""
 
     export enum State {
         //% block="Success"
@@ -39,16 +40,15 @@ namespace ESP8266_IoT {
         }
         return result
     }
-
     /**
-    * Initialize ESP8266 module and connect it to Wifi router
+    * Initialize ESP8266 module 
     */
-    //% block="Initialize ESP8266|RX (Tx of micro:bit) %tx|TX (Rx of micro:bit) %rx|Baud rate %baudrate|Wifi SSID = %ssid|Wifi PW = %pw"
+    //% block="set ESP8266|RX %tx|TX %rx|Baud rate %baudrate"
     //% tx.defl=SerialPin.P8
     //% rx.defl=SerialPin.P12
     //% ssid.defl=your_ssid
     //% pw.defl=your_pw
-    export function connectWifi(tx: SerialPin, rx: SerialPin, baudrate: BaudRate, ssid: string, pw: string) {
+    export function initWIFI(tx: SerialPin, rx: SerialPin, baudrate: BaudRate) {
         wifi_connected = false
         thingspeak_connected = false
         serial.redirect(
@@ -59,31 +59,73 @@ namespace ESP8266_IoT {
         sendAT("AT+RESTORE", 1000) // restore to factory settings
         sendAT("AT+CWMODE=1") // set to STA mode
         sendAT("AT+RST", 1000) // reset
+        basic.pause(100)
+    }
+    /**
+    * connect to Wifi router
+    */
+    //% block="connect Wifi SSID = %ssid|KEY = %pw"
+    //% ssid.defl=your_ssid
+    //% pw.defl=your_pw
+    export function connectWifi(ssid: string, pw: string) {
+        wifi_connected = false
+        thingspeak_connected = false
         sendAT("AT+CWJAP=\"" + ssid + "\",\"" + pw + "\"", 0) // connect to Wifi router
         wifi_connected = waitResponse()
         basic.pause(100)
     }
-
     /**
-    * Connect to ThingSpeak and upload data. It would not upload anything if it failed to connect to Wifi or ThingSpeak.
+    * Connect to ThingSpeak
     */
-    //% block="Upload data to ThingSpeak|URL/IP = %ip|Write API key = %write_api_key|Field 1 = %n1|Field 2 = %n2|Field 3 = %n3|Field 4 = %n4|Field 5 = %n5|Field 6 = %n6|Field 7 = %n7|Field 8 = %n8"
-    //% ip.defl=api.thingspeak.com
+    //% block="connect thingspeak"
     //% write_api_key.defl=your_write_api_key
-    export function connectThingSpeak(ip: string, write_api_key: string, n1: number, n2: number, n3: number, n4: number, n5: number, n6: number, n7: number, n8: number) {
-        if (wifi_connected && write_api_key != "") {
+    export function connectThingSpeak() {
+        if (wifi_connected) {
             thingspeak_connected = false
-            sendAT("AT+CIPSTART=\"TCP\",\"" + ip + "\",80", 0) // connect to website server
+            let text = "AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80"
+            sendAT(text, 0) // connect to website server
             thingspeak_connected = waitResponse()
             basic.pause(100)
-            if (thingspeak_connected) {
-                last_upload_successful = false
-                let str: string = "GET /update?api_key=" + write_api_key + "&field1=" + n1 + "&field2=" + n2 + "&field3=" + n3 + "&field4=" + n4 + "&field5=" + n5 + "&field6=" + n6 + "&field7=" + n7 + "&field8=" + n8
-                sendAT("AT+CIPSEND=" + (str.length + 2))
-                sendAT(str, 0) // upload data
-                last_upload_successful = waitResponse()
-                basic.pause(100)
-            }
+        }
+    }
+    /**
+    * Connect to ThingSpeak and set data. 
+    */
+    //% block="set data to send ThingSpeak|Write API key = %write_api_key|Field 1 = %n1|Field 2 = %n2|Field 3 = %n3|Field 4 = %n4|Field 5 = %n5|Field 6 = %n6|Field 7 = %n7|Field 8 = %n8"
+    //% write_api_key.defl=your_write_api_key
+    export function setdata(write_api_key: string, n1: number, n2: number, n3: number, n4: number, n5: number, n6: number, n7: number, n8: number) {
+        if (thingspeak_connected) {
+            toSendStr = "GET /update?api_key="
+            + write_api_key
+            + "&field1=" 
+            + n1
+            + "&field2=" 
+            + n2 
+            + "&field3=" 
+            + n3 
+            + "&field4=" 
+            + n4 
+            + "&field5=" 
+            + n5 
+            + "&field6=" 
+            + n6 
+            + "&field7=" 
+            + n7 
+            + "&field8=" 
+            + n8
+        }
+    }
+    /**
+    * upload data. It would not upload anything if it failed to connect to Wifi or ThingSpeak.
+    */
+    //% block="Upload data to ThingSpeak"
+    export function uploadData() {
+        if (thingspeak_connected) {
+            last_upload_successful = false
+            sendAT("AT+CIPSEND=" + (toSendStr.length + 2), 100)
+            sendAT(toSendStr, 100) // upload data
+            last_upload_successful = waitResponse()
+            basic.pause(100)
         }
     }
 
@@ -104,7 +146,7 @@ namespace ESP8266_IoT {
         if (wifi_connected == state) {
             return true
         }
-        else{
+        else {
             return false
         }
     }
@@ -114,7 +156,7 @@ namespace ESP8266_IoT {
     */
     //% block="ThingSpeak connected %State"
     export function thingSpeakState(state: boolean) {
-        if (wifi_connected == state) {
+        if (thingspeak_connected == state) {
             return true
         }
         else {
@@ -127,7 +169,7 @@ namespace ESP8266_IoT {
     */
     //% block="Last data upload %State"
     export function lastUploadState(state: boolean) {
-        if (wifi_connected == state) {
+        if (last_upload_successful == state) {
             return true
         }
         else {
